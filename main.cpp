b9ba9b861366c8524521a41ab90185680b5f76a9
李韻晟 <liyuncheng@MacBook-Pro.local>
2020-10-21 00:36:58 +0800

    refactor: refactoring buffer handling

--- main.cpp
+++ main.cpp
@@ -7,12 +7,37 @@
 #include <boost/asio.hpp>
 using namespace boost;
 
+std::vector<char> v_buffer(20 * 1024);
+
+void bufferHandler(asio::ip::socket& socket) {
+  socket.async_read_some(asio::buffer(v_buffer.data(), v_buffer.size()),
+    [&](std::error_code ec, std::size_t length) {
+      if (!ec) {
+        std::cout << "\n\nRead" << length << "\nbytes\n\n";
+
+        for (int i = 0; i < length; i++) {
+          std::cout << v_buffer[i];
+        }
+
+        bufferHandler(socket);
+      }
+    }
+  );
+}
+
 int main() {
   
   system::error_code ec;
 
   // Context Platform Specific Interface
   asio::io_context io_context;
+
+  asio::io_context::work idle_work(io_context);
+
+  // Run Context in own thread dont block main thread
+  std::thread thread_context = std::thread([&])() {
+    io_context.run();
+  }
 
   // IP endpoint & Make Address so ASIO can recognize
   asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", ec), 80);
@@ -27,26 +52,14 @@ int main() {
   }
 
   if (socket.is_open()) {
+    bufferHandler(socket);
+
     std::string s_request =
       "GET /index.html HTTP/1.1\r\n"
       "HOST: example.com\r\n"
       "Connection: close\r\n\r\n";
 
     socket.write_some(asio::buffer(s_request.data(), s_request.size()), ec);
-
-    socket.wait(socket.wait_read);
-
-    size_t bytes = socket.available();
-    std::cout << "Bytes Available: " << bytes << std::endl;
-
-    if (bytes > 0) {
-      std::vector<char> v_buffer(bytes);
-      socket.read_some(asio::buffer(v_buffer.data(), v_buffer.size()), ec);
-
-      for (auto c : v_buffer) {
-        std::cout << c;
-      }
-    }
   }
   return 0;
 }
